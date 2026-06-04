8c0a48d5e4a4004168321fdd35b102bc24281e36
Kevin G. Ferreira <kevingarciaf.kf@outlook.com>
2026-06-03 22:36:44 -0300

    create products route, controller, service and model

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -16,17 +16,79 @@ public class ProductController : ControllerBase
         _productService = productsService;
     }
 
+    // GET all products
     [HttpGet]
     public async Task<ActionResult<List<Product>>> GetAll()
     {
-        var returnedProducts = await _productService.GetProductsAsync();
-        return Ok(returnedProducts);
+        try
+        {
+            var result = await _productService.GetProductsAsync();
+            return Ok(result);
+        }
+        catch
+        {
+            return StatusCode(500, "Internal Server Error");
+        }
     }
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<Product>> GetById(Guid id)
+    // GET product by product_id
+    [HttpGet("{product_id}")]
+    public async Task<ActionResult<Product>> GetById(Guid product_id)
     {
-        var returnedById = await _productService.GetProductAsync(id);
-        return Ok(returnedById);
+        try
+        {
+            var result = await _productService.GetProductAsync(product_id);
+            return Ok(result);
+        }
+        catch
+        {
+            return StatusCode(500, "Internal Server Error");
+        }
     }
+
+    // GET products by company_id
+    [HttpGet("company/{company_id}")]
+    public async Task<ActionResult<List<Product>>> GetProductsByCompanyId(Guid company_id)
+    {
+        try
+        {
+            var result = await _productService.GetProductsByCompanyIdAsync(company_id);
+            return Ok(result);
+        }
+        catch
+        {
+            return StatusCode(500, "Internal Server Error");
+        }
+    }
+
+    // GET product by company_id
+    [HttpGet("company/{company_id}/{product_id}")]
+    public async Task<ActionResult<Product>> GetProductByCompanyId(Guid company_id, Guid product_id)
+    {
+        try
+        {
+            var result = await _productService.GetProductByCompanyIdAsync(company_id, product_id);
+            return Ok(result);
+        }
+        catch
+        {
+            return StatusCode(500, "Internal Server Error");
+        }
+    }
+    
+    // POST new product
+    [HttpPost("create")]
+    public async Task<ActionResult<Guid>> CreateProduct(Product product)
+    {
+        try
+        {
+            var newProductId =  await _productService.CreateProductAsync(product);
+            return StatusCode(201, newProductId);
+        }
+        catch
+        {
+            return StatusCode(500, "Internal Server Error");
+        }
+    }
+
 }
--- API/Services/ProductService.cs
+++ API/Services/ProductService.cs
@@ -12,6 +12,7 @@ public class ProductService
         _context = context;
     }
 
+    // GET All products
         public async Task<List<Product>> GetProductsAsync()
     {
         return await _context.products
@@ -20,9 +21,46 @@ public class ProductService
             .ToListAsync();
     }
 
-    public async Task<Product?> GetProductAsync(Guid id)
+    // GET Product by product_id
+    public async Task<Product?> GetProductAsync(Guid product_id)
     {
         return await _context.products
-            .FirstOrDefaultAsync(p => p.id == id);
+            .Include(p => p.Category)
+            .Include(p => p.Company)
+            .FirstOrDefaultAsync(p => p.id == product_id);
     }
+
+    // GET Products by company_id
+    public async Task<List<Product>> GetProductsByCompanyIdAsync(Guid company_id)
+    {
+        return await _context.products
+            .Include(p => p.Category)
+            .Include(p => p.Company )
+            .Where(p => p.company_id == company_id)
+            .ToListAsync();
+    }
+
+    // GET product by company_id
+    public async Task<Product?> GetProductByCompanyIdAsync(Guid company_id, Guid product_id)
+    {
+        return await _context.products
+            .Include(p => p.Category)
+            .Include(p => p.Company)
+            .Where(p => p.company_id == company_id)
+            .FirstOrDefaultAsync(p => p.id == product_id);
+
+    }
+
+    // POST (create) product
+    public async Task<Guid> CreateProductAsync(Product product)
+    {
+        product.id = Guid.NewGuid();
+        await _context.products.AddAsync(product);
+        await _context.SaveChangesAsync();
+        
+        return product.id;
+    } 
+
+
+
 }
